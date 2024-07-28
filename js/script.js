d14819739f08b2f62d6cd3cf90d207f573a694bf
ngys9919 <ngys9919@yahoo.com>
2024-07-28 19:05:46 +0800

    Add SuperUser

--- js/script.js
+++ js/script.js
@@ -53,21 +53,17 @@ document.addEventListener("DOMContentLoaded", async function(){
       renderList(books);
   });
 
-  // let addBtn = document.querySelector("#addBtn");
-  // addBtn.addEventListener('submit', function(){
-    // let isbn = document.querySelector("#isbn").value;
-    // let title = document.querySelector("#title").value;
-    // let author = document.querySelector("#author").value;
-    // addBook(books, title, author, isbn);
-    // renderList(books);
-  // });
+  let passwordBtn = document.querySelector("#passwordBtn");
+  passwordBtn.addEventListener('click', function(){
+    ret = checkPassword();
+    if (ret == true) {
+      renderListSuperUser(books);
+    } else {
+      renderList(books);      
+    }
+  });
   
-  // let saveBtn = document.querySelector("#saveBtn");
-  // saveBtn.addEventListener("click", function(){
-    // saveBooks(books);
-  // });
-  
-   function renderList(books) {
+  function renderList(books) {
     let bookListDiv = document.querySelector("#bookList");
     let outputString = "";
     if (books.length === 0) {
@@ -84,48 +80,82 @@ document.addEventListener("DOMContentLoaded", async function(){
       // sample output:
       // `<li>ISBN: 9781593275402, Title: The Principles of Object-Oriented JavaScript, Author: Nicholas C. Zakas, isCheckedOut: false</li>`
       outputString += `<li>ISBN: ${listIndex.isbn}, Title: ${listIndex.title}, Author: ${listIndex.author}, Checked-Out: ${listIndex.isCheckedOut}
-        <button class="editBtn btn-success" data-isbn="${listIndex.isbn}">Edit</button>
-        <button class="deleteBtn btn-danger" data-isbn="${listIndex.isbn}">Delete</button>
+        <button class="editBtn btn-success" data-isbn="${listIndex.isbn}" disabled>Edit</button>
+        <button class="deleteBtn btn-danger" data-isbn="${listIndex.isbn}" disabled>Delete</button>
       </li>`;
     }
-    bookListDiv.innerHTML = outputString;
-     // after we set the inner HTML, all the elements will be created 
-     // all buttons will be created, so we use querySelectorAll on all of them
-     
-     // EDIT BUTTONS
-     let allEditButtons =  document.querySelectorAll(".editBtn");
-     for (let button of allEditButtons) {
-      button.addEventListener("click", function(event) {
-      // the first parameter for a function handling an event is the event info
-      let clickedButton = event.target;
-      // let isbn = Number(clickedButton.dataset.isbn);
-      let isbn = clickedButton.dataset.isbn;
-      // let isbn = prompt("Enter the new isbn");
-      let title = prompt("Enter the new title");
-      let author = prompt("Enter the new author");
-      editBook(books, title, author, isbn);
-      saveBooks(books);
-      renderList(books); 
-      });
-     }
+    bookListDiv.innerHTML = outputString; 
+  }
+
+  function checkPassword() {
+       var password = document.getElementById("passwordBox");
+       var passwordText = password.value;
+       if(passwordText == "Ernest300") {
+        return true;
+       }
+       alert("Access denied! Incorrect password!");
+       return false;
+  }
+
+  function renderListSuperUser(books) {
+      let bookListDiv = document.querySelector("#bookList");
+      let outputString = "";
+      if (books.length === 0) {
+        outputString = "";
+        bookListDiv.innerHTML = outputString;
+        return;
+      }
   
-     // DELETE BUTTONS
-     let allDeleteButtons = document.querySelectorAll(".deleteBtn");
-     for (let button of allDeleteButtons) {
-       button.addEventListener("click", function(event){
-        const toDelete = confirm("Are you sure you want to delete?");
-        if (toDelete) {
-         // get the book id
-        //  let isbn = Number(event.target.dataset.isbn);
-         let isbn = event.target.dataset.isbn;
-         deleteBook(books, isbn);
-         saveBooks(books);
-         renderList(books);
-        }
-       })
-     }
+      // render the list
+      console.log(books);
+      console.log("===== Booklist Summary =====");
+      for (let listIndex of books) {
+        console.log(`ISBN: ${listIndex.isbn}, Title: ${listIndex.title}, Author: ${listIndex.author}, Checked-Out: ${listIndex.isCheckedOut}`);
+        // sample output:
+        // `<li>ISBN: 9781593275402, Title: The Principles of Object-Oriented JavaScript, Author: Nicholas C. Zakas, isCheckedOut: false</li>`
+        outputString += `<li>ISBN: ${listIndex.isbn}, Title: ${listIndex.title}, Author: ${listIndex.author}, Checked-Out: ${listIndex.isCheckedOut}
+          <button class="editBtn btn-success" data-isbn="${listIndex.isbn}" enabled>Edit</button>
+          <button class="deleteBtn btn-danger" data-isbn="${listIndex.isbn}" enabled>Delete</button>
+        </li>`;
+      }
+      bookListDiv.innerHTML = outputString;
+       // after we set the inner HTML, all the elements will be created 
+       // all buttons will be created, so we use querySelectorAll on all of them
+       
+       // EDIT BUTTONS
+       let allEditButtons =  document.querySelectorAll(".editBtn");
+       for (let button of allEditButtons) {
+        button.addEventListener("click", function(event) {
+        // the first parameter for a function handling an event is the event info
+        let clickedButton = event.target;
+        // let isbn = Number(clickedButton.dataset.isbn);
+        let isbn = clickedButton.dataset.isbn;
+        // let isbn = prompt("Enter the new isbn");
+        let title = prompt("Enter the new title");
+        let author = prompt("Enter the new author");
+        editBook(books, title, author, isbn);
+        saveBooks(books);
+        renderListSuperUser(books); 
+        });
+       }
+    
+       // DELETE BUTTONS
+       let allDeleteButtons = document.querySelectorAll(".deleteBtn");
+       for (let button of allDeleteButtons) {
+         button.addEventListener("click", function(event){
+          const toDelete = confirm("Are you sure you want to delete?");
+          if (toDelete) {
+           // get the book id
+          //  let isbn = Number(event.target.dataset.isbn);
+           let isbn = event.target.dataset.isbn;
+           deleteBook(books, isbn);
+           saveBooks(books);
+           renderListSuperUser(books);
+          }
+         })
+       }
     }
-  
+
   // async function main() {
     // books = await loadBooks();
     // renderList(books);
